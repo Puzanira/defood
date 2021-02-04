2658b6b96807ac62c27e045e7977c6ad83e5ee9c
Sarafa2n <slaft3@yandex.ru>
2021-02-04 20:14:34 +0300

    Сделать базовую архитектуру проекта

---
+++ src/components/app/app.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { RouterStore } from '../../store/routes';
+
+import IndexLayout from "../layouts/index-layout/index-layout";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const route = (path, layout, exact = true) => ({path, layout, exact});
+
+    this.routes = [
+        route(RouterStore.website.index, <IndexLayout />),
+    ];
+
+  }
+
+  render() {
+      const routes = this.routes.map((route, idx) => {
+          const { path, layout, exact } = route;
+          return <Route key={idx} path={path} exact={exact} render={() => layout} />;
+      });
+
+    return(
+        <BrowserRouter>
+            <div className={'component-app'}>
+                <Switch>
+                    { routes }
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+  }
+
+}
+export default App;
---
+++ src/store/routes.js
@@ -0,0 +1,5 @@
+export const RouterStore =  {
+    website: {
+        index: '/',
+    },
+};
