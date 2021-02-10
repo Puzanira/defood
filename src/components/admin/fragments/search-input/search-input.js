5f447a1f4fa313857dac6275d4a98306372a3887
Nell <ADshishova@mail.ru>
2021-02-10 22:23:41 +0300

    added state for search and filters

--- src/components/admin/fragments/search-input/search-input.js
+++ src/components/admin/fragments/search-input/search-input.js
@@ -11,25 +11,48 @@ import SearchFilterListFragment from '../../fragments/search-filter-list/search-
  */
 function SearchInputFragment({data}) {
     const searchFilters = [
-        [{value: '0', text: 'Статус'}, {value: '1', text: 'Принят'}, {value: '2', text: 'Готов'}, {value: '3', text: 'В пути'}, {value: '4', text: 'Завершен'}],
-        [{value: '0', text: 'Стоимость'}, {value: '1', text: 'По возрастанию'}, {value: '2', text: 'По убыванию'}],
-        [{value: '0', text: 'Время'}, {value: '1', text: 'Ближайшие'}, {value: '2', text: 'Давнейшие'}],
+        {
+            id: 'Статус',
+            array: [{value: '0', text: 'Статус'}, {value: '1', text: 'Принят'}, {value: '2', text: 'Готов'}, {value: '3', text: 'В пути'}, {value: '4', text: 'Завершен'}]
+        },
+        {
+            id: 'Стоимость',
+            array: [{value: '0', text: 'Стоимость'}, {value: '1', text: 'По возрастанию'}, {value: '2', text: 'По убыванию'}]
+        },
+        {
+            id: 'Время',
+            array: [{value: '0', text: 'Время'}, {value: '1', text: 'Ближайшие'}, {value: '2', text: 'Давнейшие'}]
+        },
     ];
 
     const initialState = {
         dataComp: data,
+
         searchFiltersTexts: searchFilters,
+        isOpenFilters: false,
+        filtersState: searchFilters.reduce((acc, elem) => {
+            acc[elem.id] = 0;
+            return acc;
+        }, {}),
+
+        searchBarValue: '',
     };
 
     const changeField = (field, value) => {
         dispatch({type: 'CHANGE_FIELD', field, value});
     };
 
+    const changeFilterField = (field, value) => {
+        dispatch({type: 'CHANGE_FILTER_FIELD', field, value});
+    }
+
     const [state, dispatch] = useReducer(
         (state, action) => {
             switch (action.type) {
                 case 'CHANGE_FIELD':
                     return {...state, [action.field]: action.value};
+                case 'CHANGE_FILTER_FIELD':
+                    return {...state, filtersState: {...state.filtersState, [action.field]: action.value}};
                 default:
                     return state;
             }
@@ -40,21 +63,43 @@ function SearchInputFragment({data}) {
     const {
         dataComp,
         searchFiltersTexts,
+        isOpenFilters,
+        filtersState,
+        searchBarValue,
     } = state;
+
+    const changeFiltersHandler = () => {
+        if (isOpenFilters) {
+            changeField('isOpenFilters', false);
+            Object.keys(filtersState).forEach(elem => {
+                changeFilterField(elem, 0);
+            });
+        } else {
+            changeField('isOpenFilters', true);
+        }
+    }
 
     return (
         <div className="search-input">
             <div className="search-input__line">
-                <SearchBar placeholder='Поиск' className='search-bar'/>
-                <div className="search-input__filters-btn">
+                <SearchBar 
+                    placeholder='Поиск' 
+                    className='search-bar'
+                    value={searchBarValue}
+                    onChange={(value) => changeField('searchBarValue', value)}
+                    onCancelSearch={(value) => changeField('searchBarValue', '')}
+                />
+                <div className="search-input__filters-btn" onClick={() => changeFiltersHandler()}>
                     Фильтры
                 </div>
             </div>
-            <div className="search-input__filters">
-                {searchFiltersTexts.map(item => (
-                    <SearchFilterListFragment data={item}/>
-                ))}
-            </div>
+            {isOpenFilters && (
+                <div className="search-input__filters">
+                    {searchFiltersTexts.map((item, index) => (
+                        <SearchFilterListFragment data={item.array} changeFilterField={changeFilterField} key={index}/>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
