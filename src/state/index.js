16e1235cc5d58e7c5282585538f82e27f5a65b86
Puzanira <puzanova.ira2011@yandex.ru>
2021-02-18 11:36:26 +0300

    Removed .eslintcache and repo

--- src/state/index.js
+++ src/state/index.js
@@ -1,11 +1,9 @@
 import { mergeStateParts } from './utils';
-import { repo } from './repo';
 import { client } from './client';
 import { admin } from './admin';
 
 
 export const state = mergeStateParts('main', {
-    repo,
     client,
     admin,
 });
